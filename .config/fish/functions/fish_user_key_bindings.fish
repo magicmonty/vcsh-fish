21acffacd06dc5af3c113c51e414be0038e27af1
Martin Gondermann <magicmonty@pagansoft.de>
2017-08-29 22:10:57 +0200

    Update

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -35,6 +35,7 @@ function fish_user_key_bindings
   bind \cl 'clear; commandline -f repaint'
   bind \cu backward-kill-line
   bind \cr search_history
+  bind -M insert \co edit_config_fish
   bind \co edit_config_fish
 
 end
